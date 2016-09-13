8463e62f41f3513aad8b306434f943168bfce7be
Chaowen Chang <kevin68x@gmail.com>
2016-09-14 05:55:00 +0800

    insert google analytics code and refresh reading indicator

--- themes/mirror/static/js/article.js
+++ themes/mirror/static/js/article.js
@@ -2,39 +2,54 @@ var progressBar, max, value, width;
 $(document)
   .ready(function() {
 
-    var getMax = function() {
-      return $(document).height() - $(window).height();
-    }
+    // var getMax = function() {
+    //   return $(document).height() - $(window).height();
+    // }
 
-    var getValue = function() {
-      return $(window).scrollTop();
-    }
+    // var getValue = function() {
+    //   return $(window).scrollTop();
+    // }
 
-    progressBar = $('.progress'),
-      max = getMax(),
-      value, width;
+    // progressBar = $('.progress'),
+    //   max = getMax(),
+    //   value, width;
 
-    var getWidth = function() {
-      // Calculate width in percentage
-      value = getValue();
-      width = (value / max) * 100;
-      width = width;
-      return width;
-    }
+    // var getWidth = function() {
+    //   // Calculate width in percentage
+    //   value = getValue();
+    //   width = (value / max) * 100;
+    //   width = width;
+    //   return width;
+    // }
 
-    var setWidth = function() {
-      console.log(width);
-      progressBar.progress({
-        percent: getWidth()
+    // var setWidth = function() {
+    //   console.log(width);
+    //   progressBar.progress({
+    //     percent: getWidth()
+    //   });
+    // }
+
+    // $(document).on('scroll', setWidth);
+    // $(window).on('resize', function() {
+    //   // Need to reset the Max attr
+    //   max = getMax();
+    //   setWidth();
+    // });
+
+    // Reading Progress Indicator
+    var updateProgress = function () {
+      // calculate values
+      var scroll = $(window).scrollTop();
+      var height = $(document).height() - $(window).height();
+      var percent = Math.round(scroll * 100 / height);
+
+      // update progress count
+      $('.progress').progress({
+        percent: percent
       });
     }
-
-    $(document).on('scroll', setWidth);
-    $(window).on('resize', function() {
-      // Need to reset the Max attr
-      max = getMax();
-      setWidth();
-    });
+    updateProgress();
+    $(window).scroll(updateProgress);
 
     // fix main menu to page on passing
     $('.main.menu').visibility({
@@ -71,4 +86,6 @@ $(document)
       ;
     });
 
+    // silent debug messages
+    $.site('change setting', 'silent', true);
   });
