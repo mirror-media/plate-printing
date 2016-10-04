3f3556254b8518334a0705fd8bfda797d4a5b62c
Chaowen Chang <imkojima@me.com>
2016-10-04 18:01:33 +0800

    breakline in blockquote

--- themes/mirror/static/js/article-component.js
+++ themes/mirror/static/js/article-component.js
@@ -51,6 +51,7 @@ $(document)
   			$(blockquote).addClass('blockquote');
   			var quoteBody = $(blockquote).find('div:nth-child(1)');
   			var quoteBy = $(blockquote).find('div:nth-child(2)');
+        quoteBody.html( quoteBody.html().replace(/\n/g, "<br/>") );
   			quoteBody.addClass('quote-body');
   			quoteBy.addClass('quote-by');
   		} else {
