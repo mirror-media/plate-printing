6e2536ae066a8766b341d1248ce2ee59ca5fa9a6
Chaowen Chang <imkojima@me.com>
2016-11-30 16:00:30 +0800

    implement play button on hero video

--- themes/mirror/static/js/article-component.js
+++ themes/mirror/static/js/article-component.js
@@ -52,6 +52,16 @@ $(document)
       });
     }
 
+    $('.video').parent().click(function () {
+      if($(this).children(".video").get(0).paused){
+        $(this).children(".video").get(0).play();
+        $(this).children(".playpause").fadeOut();
+      }else{
+        $(this).children(".video").get(0).pause();
+        $(this).children(".playpause").fadeIn();
+      }
+    });
+
     /* Components */
     $('article div > img').each(function(i, img) {
       var alt = img.alt;
